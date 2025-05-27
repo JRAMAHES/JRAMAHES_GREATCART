633e9f59485d74d2cd43c79ab946d3f9cdd46b37
JRAMAHE'S <juanramhdiconce@gmail.com>
2025-05-27 11:42:37 -0700

    JRAMAHES first commit of the project GreatCart

---
+++ greatkart/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from product.models import Product
+
+
+def home(request): 
+    # Filter products that are available and not expired
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'index.html', context)
---
+++ product/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
+    list_display = ('product_name', 'price', 'stock', 'category', 'is_available', 'created_at', 'modified_at')
+
+admin.site.register(Product, ProductAdmin)
